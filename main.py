8afcc36fae48e3a38683e28a14ebe6556136cb06
Arno Baumfalk <a.baumfalk@web.de>
2023-09-29 18:45:30 +0200

    deduplication

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ from functools import reduce
 from pathlib import Path
 
 
-class SyllableGen:
-    def __init__(self, path):
-        self.syllables = {}
+class RandomGenerator:
+    def __init__(self):
+        self.item_count = {}
         self.sum = 0
+
+    def get(self):
+        num = random.randrange(self.sum)
+        for item, count in self.item_count.items():
+            num -= count
+            if num <= 0:
+                return item
+
+        # this should never happen
+        raise RuntimeError("out of range")
+
+
+class SyllableGen(RandomGenerator):
+    def __init__(self, path):
+        super().__init__()
+
         with open(path) as f:
             for line in f.readlines():
                 if line.startswith('#'):
@@ -22,25 +38,14 @@ class SyllableGen:
                 syllable_count = len(syllables)
                 add = count // syllable_count
                 for syllable in syllables:
-                    self.syllables[syllable] = add
+                    self.item_count[syllable] = add
                 # correct for integer rounding
-                self.syllables[syllable] += count - add * syllable_count
-
-    def get(self):
-        num = random.randrange(self.sum)
-        for syllable, count in self.syllables.items():
-            num -= count
-            if num <= 0:
-                return syllable
-
-        # this should never happen
-        raise RuntimeError("out of range")
+                self.item_count[syllable] += count - add * syllable_count
 
 
-class WordLenGen:
+class WordLenGen(RandomGenerator):
     def __init__(self, path):
-        self.length_probability = {}
-        self.sum = 0
+        super().__init__()
 
         with open(path) as f:
             for line in f.readlines():
@@ -49,22 +54,12 @@ class WordLenGen:
 
                 syllable_count, occurrence, _ = line.split(maxsplit=3)
                 syllable_count = int(syllable_count)
-                if syllable_count in self.length_probability:
+                if syllable_count in self.item_count.keys():
                     raise RuntimeError(f"duplicate syllable count {syllable_count}")
 
                 occurrence = int(occurrence)
-                self.length_probability[syllable_count] = occurrence
+                self.item_count[syllable_count] = occurrence
                 self.sum += occurrence
-
-    def get(self):
-        num = random.randrange(self.sum)
-        for length, count in self.length_probability.items():
-            num -= count
-            if num <= 0:
-                return length
-
-        # this should never happen
-        raise RuntimeError("out of range")
 
 
 class IPATranscriptor:
